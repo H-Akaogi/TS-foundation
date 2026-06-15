316bde2426c7f044aa4091979e47485b5414b64c
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-15 11:46:07 +0900

    演習3-1 入力して文字列をnumber型に変換する

---
+++ dist/chap03/ex3-1.d.ts
@@ -0,0 +1,2 @@
+export declare function ex31_func(): void;
+//# sourceMappingURL=ex3-1.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// 定数messageを宣言する
-const message: string = "Hello TypeScript";
-// message定数をコンソール出力する
-console.log(message);
+// chap03フォルダのex3-1.jsから、ex31_func関数を読み込む
+import { ex31_func } from './chap03/ex3-1.js';
+// ex31_func()関数を実行する
+ex31_func();
