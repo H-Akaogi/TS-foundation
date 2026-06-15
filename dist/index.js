8b721063bf1e8231eb7de58a9bd08bcb34368fd6
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-15 11:02:04 +0900

    演習2-3 メッセージを表示するプログラムをコンパイルして実行する

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+// 定数messageを宣言する
+const message = "Hello TypeScript";
+// message定数をコンソール出力する
+console.log(message);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// 定数messageを宣言する
+const message: string = "Hello TypeScript";
+// message定数をコンソール出力する
+console.log(message);
