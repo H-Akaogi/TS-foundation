888c5abdb9d7e6bd042e31a8afb4f13f8567fc37
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-22 16:56:27 +0900

    演習4-3 デフォルト引数を持つ関数を作成して利用する

---
+++ dist/chap04/ex4-3.d.ts
@@ -0,0 +1,11 @@
+/**
+ * 演習4-3 デフォルト引数を持つ関数を作成して利用する
+ */
+/**
+ * 指定された率の値引きをした単価を計算して返す
+ * @param price 単価
+ * @param discountRate 値引き率(デフォルト引数)
+ * @returns 値引き後単価
+ */
+export declare function calcPrice(price: number, discountRate?: number): number;
+//# sourceMappingURL=ex4-3.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,22 @@ import PromptSync from "prompt-sync";
 // ユーザー入力を受け取るための準備(sigint: trueはCtrl+C で終了できるようにする設定）
 const prompt = PromptSync({ sigint: true });
 // chap04フォルダのex4-1.jsから、calcPrice関数を読み込む
-import { calcPrice } from "./chap04/ex4-2.js";
+import { calcPrice } from "./chap04/ex4-3.js";
 
 // キー入力された値を取得する
 let value: string = prompt(`金額を入力してください-> `);
 // 文字列を数値に変換する
 const price = parseInt(value);
 // キー入力された値を取得する
-value = prompt("値引き率(0.1~0.9)を入力してください-> ");
+value = prompt("値引き率(0~0.9)を入力してください-> ");
 const discountRate = parseFloat(value);
-// 指定された率の値引きをした金額を計算する
-const discountPrice = calcPrice(price, discountRate);
 
 console.log(`金額; ${price}`);
-console.log(`値引き率:${discountRate}`);
-console.log(`値引き後金額: ${discountPrice}`);
+if (discountRate === 0) {
+    console.log(`値引き率:${0.2}`);
+    console.log(`値引き後金額:${calcPrice(price)}`);
+}
+else {
+    console.log(`値引き率:${discountRate}`);
+    console.log(`値引き後金額:${calcPrice(price, discountRate)}`);
+}
