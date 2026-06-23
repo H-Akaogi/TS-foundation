d07b58cf7907d4979427442d9c35250611f67c6c
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-23 11:42:46 +0900

    List 5-4 メソッド

--- src/chap05/Character.ts
+++ src/chap05/Character.ts
@@ -23,4 +23,23 @@ export class Character {
         this.name = name;
         this.hp = hp;
     }
+
+    /**
+     * 自己紹介をするメソッド
+     * functionキーワードはつけない
+     */
+    introduce(): void {
+        console.log(`私は${this.name}。体力は残り${this.hp}です。`);
+    }
+
+    /**
+     * ダメージを受けるメソッド
+     * functionキーワードはつけない
+     * @param damage ダメージ
+     */
+    takeDamage(damage: number): void {
+        this.hp -= damage; // 自分のHPを減らす
+        if (this.hp < 0) this.hp = 0;
+        console.log(`${this.name}は${damage}のダメージを受けた！`);
+    }
 }
