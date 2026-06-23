0c858933392435743e24e4da6b0fce47cde533f9
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-23 12:16:09 +0900

    List 5-7 クラスの継承(コンストラクタ)

---
+++ src/chap05/Warrior.ts
@@ -0,0 +1,20 @@
+import { Character } from "./Character.js";
+
+export class Warrior extends Character {
+    // 武器を表すプロパティ
+    private _weapon: string;
+
+    /**
+     * コンストラクタ
+     * @param name 名前
+     * @param hp 体力
+     * @param weapon 武器
+     */
+    constructor(name: string, hp: number, weapon: string) {
+        // 親クラスのコンストラクタを呼び出して、親の部分を完成させる
+        super(name, hp);
+        // 子クラス独自の初期化を行う
+        this._weapon = weapon;
+    }
+}
+
